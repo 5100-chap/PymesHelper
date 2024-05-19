737a0d1bde8edc8ff58c34cb1284071e5c052fc2
Marcelo91216 <75495866+Marcelo91216@users.noreply.github.com>
2024-05-18 18:18:12 -0600

    back.py con la base creada, lea el readme para más información

--- Backend-webapp/back.py
+++ Backend-webapp/back.py
@@ -1,16 +1,25 @@
 from flask import Flask, request, jsonify
+# from ..API.models.MLmodel import MLmodel
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST"])
+image_extensions = ['jpg', 'jpeg', 'png', 'svg']
+
+@app.route("/", methods=["GET"])
 def peticion():
     return "<span>HOLA</span>"
 
 @app.route("/file", methods=["POST"])
 def send_file():
-    file = request.files["file"]
-    print(f"The name of the file: {file.filename}")
-    return f"<span>Petición mandada!!</span><br><span style='blue;'>{file.filename}</span>"
+    try:
+        file = request.files["file"]
+        # print(f"The name of the file: {file.filename}")
+        # print(f"{file.filename.split('.')[1]}")
+        if file.filename.split('.')[1] not in image_extensions:
+            raise Exception(f"Debe utilizar un formato de imagen adecuado, los aceptados son: {image_extensions}")
+        return f"Respuesta del modelo: work in progress..."
+    except Exception as e:
+        return jsonify({"error": str(e)}), 404
 
 if __name__=="__main__":
     app.run(debug=True, threaded=True)
