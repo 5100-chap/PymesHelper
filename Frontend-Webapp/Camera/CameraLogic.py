1e27d2d520dac060b20fe8f19940ed0cfa13ce33
5100-chap <A01275100@tec.mx>
2024-05-28 16:02:28 -0600

    TOFIX: camara

--- Frontend-Webapp/Camera/CameraLogic.py
+++ Frontend-Webapp/Camera/CameraLogic.py
@@ -1,9 +1,10 @@
 import cv2
-
+from streamlit_webrtc import VideoTransformerBase
 class CameraLogic:
     def __init__(self):
         pass
-
+    
+    #Cambiar a logica de webrtc
     def list_cameras(self):
         available_cameras = []
         for i in range(10):
@@ -12,3 +13,16 @@ class CameraLogic:
                 available_cameras.append(i)
                 cap.release()
         return available_cameras
+
+class VideoTransformer(VideoTransformerBase):
+    def __init__(self, camera_processor):
+        self.camera_processor = camera_processor
+
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        # Procesar el frame aquí
+        classification = self.camera_processor.send_video(img)
+        processed_frame = self.camera_processor.process_frame(img)
+
+        return cv2.cvtColor(processed_frame, cv2.COLOR_BGR2RGB)
--- Frontend-Webapp/frontend.py
+++ Frontend-Webapp/frontend.py
@@ -3,8 +3,10 @@ import cv2
 import time
 import dotenv
 import os
-from Camera.CameraLogic import CameraLogic
+from Camera.CameraLogic import VideoTransformer
 from Camera.CameraProcessor import CameraProcessor
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, VideoHTMLAttributes
+
 
 dotenv.load_dotenv()
 
@@ -15,56 +17,38 @@ response_placeholder = st.empty()
 classification_box = st.empty()
 frame_count = 0
 
-camera_logic = CameraLogic()
 camera_processor = CameraProcessor(api_url=os.getenv("API_URL"))
 
-cameras = camera_logic.list_cameras()
+webrtc_ctx = webrtc_streamer(
+    key="example",
+    mode=WebRtcMode.SENDRECV,
+    video_processor_factory=VideoTransformer(camera_processor=camera_processor),
+    media_stream_constraints={"video": True, "audio": False},
+    video_html_attrs=VideoHTMLAttributes(
+        autoPlay=True,
+        controls=False,
+        style={"width": "100%"},
+    ),
+)
 
-if len(cameras) == 0:
-    st.error("No se encontraron cámaras.")
-else:
-    selected_camera = st.selectbox("Selecciona una cámara", cameras)
-    
-    if selected_camera is not None:
-        if st.button("Empezar", key="start_button"):
-            cap = cv2.VideoCapture(selected_camera)
+if webrtc_ctx.video_processor:
+    webrtc_ctx.video_processor.camera_processor = camera_processor
 
-            if not cap.isOpened():
-                st.error(f"No se pudo abrir la cámara {selected_camera}.")
-            else:
-                stop_processing = False
-                
-                stop_button = st.button("Parar", key="stop_button")
-                ##save_button = st.button("Guardar resultados")
-                
-                while cap.isOpened() and not stop_processing:
-                    try:
-                        ret, frame = cap.read()
+    while True:
+        try:
+            frame = webrtc_ctx.video_processor.recv()
 
-                        if ret:
-                            classification = camera_processor.send_video(frame)
-                            processed_frame = camera_processor.process_frame(frame)
+            if frame is not None:
+                classification = camera_processor.send_video(frame.to_ndarray(format="bgr24"))
+                processed_frame = camera_processor.process_frame(frame.to_ndarray(format="bgr24"))
 
-                        frame_placeholder.image(cv2.cvtColor(processed_frame, cv2.COLOR_BGR2RGB), channels="RGB")
-                        classification_box.text_area("El modelo detectó que esto es:", value=classification, height=100, key=f"classification_{frame_count}")
-                        frame_count += 1
-                        
-                        if stop_button:
-                            stop_processing = True
-                        
-                        ##if save_button:
-                        ##    with open("resultados.txt", "w") as file:
-                        ##        file.write(classification)
-                        ##    st.success("Resultados guardados en resultados.txt")
-                            
-                    except Exception as e:
-                        st.error(f"Ocurrió un error: {str(e)}")
-                        break
-                        
-                cap.release()
-                
-                new_selected_camera = st.selectbox("Selecciona una nueva cámara", cameras, key="new_camera")
-                selected_camera = new_selected_camera
-                
-                time.sleep(5)
-                st.experimental_rerun()
+                frame_placeholder.image(cv2.cvtColor(processed_frame, cv2.COLOR_BGR2RGB), channels="RGB")
+                classification_box.text_area("El modelo detectó que esto es:", value=classification, height=100, key=f"classification_{frame_count}")
+                frame_count += 1
+
+        except Exception as e:
+            st.error(f"Ocurrió un error: {str(e)}")
+            break
+
+        if st.button("Parar", key="stop_button"):
+            break
